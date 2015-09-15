f7ab93831e30c1760bf9263b75f552e05a8c675e
Didrik <didrikpa@gmail.com>
2015-09-15 15:26:46 +0200

    Create ex1.s

---
+++ ex1.s
@@ -0,0 +1,157 @@
+        .syntax unified
+	
+	      .include "efm32gg.s"
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  // Exception vector table
+  // This table contains addresses for all exception handlers
+	//
+	/////////////////////////////////////////////////////////////////////////////
+	
+        .section .vectors
+	
+	      .long   stack_top               /* Top of Stack                 */
+	      .long   _reset                  /* Reset Handler                */
+	      .long   dummy_handler           /* NMI Handler                  */
+	      .long   dummy_handler           /* Hard Fault Handler           */
+	      .long   dummy_handler           /* MPU Fault Handler            */
+	      .long   dummy_handler           /* Bus Fault Handler            */
+	      .long   dummy_handler           /* Usage Fault Handler          */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* SVCall Handler               */
+	      .long   dummy_handler           /* Debug Monitor Handler        */
+	      .long   dummy_handler           /* Reserved                     */
+	      .long   dummy_handler           /* PendSV Handler               */
+	      .long   dummy_handler           /* SysTick Handler              */
+
+	      /* External Interrupts */
+	      .long   dummy_handler
+	      .long   gpio_handler            /* GPIO even handler */
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   gpio_handler            /* GPIO odd handler */
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+	      .long   dummy_handler
+
+	      .section .text
+
+	/////////////////////////////////////////////////////////////////////////////
+	//
+	// Reset handler
+  // The CPU will start executing here after a reset
+	//
+	/////////////////////////////////////////////////////////////////////////////
+
+	      .globl  _reset
+	      .type   _reset, %function
+        .thumb_func
+_reset: 
+
+//Initialize GPIO clock
+
+	//Load CMU base address.
+	ldr r1, =CMU_BASE
+
+	//Load current value of HFPERCLKEN0
+	ldr r2,[r1, #CMU_HFPERCLKEN0]
+	
+	//Set bit for GPIO clk
+	mov r3, #1
+	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+	orr r2, r2, r3
+
+	//Store new value
+	str r2, [r1, #CMU_HFPERCLKEN0]
+
+// Set up port A and port C
+	ldr r0, =GPIO_PA_BASE
+	ldr r1, =GPIO_PC_BASE
+
+// Set high drive strength for LED
+	mov r3, #0x2
+	str r3, [r0, GPIO_CTRL]
+
+// Set LED's to light
+	ldr r5, =0x00000000
+	str r5, [r0, #GPIO_MODEH]
+
+// Set up buttons for pushing
+	ldr r4, =0x33333333
+	str r4, [r1, #GPIO_MODEL]
+	
+//
+	ldr r6, =0xff
+	str r6, [r1, #GPIO_DOUT]
+
+//
+
+	str r1, [r5, #GPIO_DIN]
+	
+
+	
+	
+
+
+
+	      
+	     
+	
+	      
+	      
+	      
+	   
+
+	
+	/////////////////////////////////////////////////////////////////////////////
+	//
+  // GPIO handler
+  // The CPU will jump here when there is a GPIO interrupt
+	//
+	/////////////////////////////////////////////////////////////////////////////
+	
+        .thumb_func
+gpio_handler:  
+
+	      b .  // do nothing
+	
+	/////////////////////////////////////////////////////////////////////////////
+	
+        .thumb_func
+dummy_handler:  
+        b .  // do nothing
